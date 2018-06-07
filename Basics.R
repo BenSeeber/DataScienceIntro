15459411edeecfc019665cebcd5be812d0b3a7ef
Benjamin Seeber <Benjamin.Seeber@five1.de>
2018-06-07 14:28:08 +0200

    More information is added

--- Basics.R
+++ Basics.R
@@ -7,3 +7,57 @@ install.packages('xgboost')
 #Dann können diese jetzt auch geladen werden
 library(rbokeh)
 library(xgboost)
+
+#Wie erfolgt die Referenzierung in R? Ganz einfach mit einem <- 
+#Dann kann man einfach einer Variablen einen Character zuweisen
+ich_bin_ein_string <- 'LoL'
+aber_ich_bin_der_geilere_strin <- "LoLLL"
+
+#Dann sind auch Integer gar nicht so schwer
+Ganzzahl <- 42
+#Was war ich noch mal?
+typeof(Ganzzahl)
+#Oh doch keine Ganzzahl, dann noch mal
+Ganzzahl <- as.integer(Ganzzahl)
+typeof(Ganzzahl)
+#Jetzt aber, und ja ich kann mich selber überschreiben
+sprintf("Ich bin ein %s",typeof(Ganzzahl))
+#Ja geil jetzt wissen es alle
+
+#So jetzt mal ein Vektor
+cooler_vector <- c('ich','bin','ein','vecotr')
+#und ich kann auch anders
+cooler_vector[1]
+
+#mit dem
+cooler_vector_zwei <- c('Ich','bin','noch','geiler','Dude')
+
+#gibt es ein data.frame
+gang <- data.frame(cooler_vector,cooler_vector_zwei)
+#ups... muss wohl einer gehen, füße ab
+gang <- data.frame(cooler_vector,head(cooler_vector_zwei,-1))
+
+#jetzt noch die Namen
+colnames(gang) <- c('Coole','Nase')
+
+#So das war das wichtigste
+#oh noch schnell mal was einlesen
+aus_nem_csv <- read.csv('~/test.csv',sep = ",")
+
+#Ach ja, evtl. braucht es noch einmal den Typ Matrix aus einem Dataframe
+data.matrix(gang)
+
+#Ach und wer auch mal was mit einer Funktion machen möchte
+geilefunktion <- function(input) 
+  {
+  for (i in input)
+    {
+    print(i)
+    }
+}
+
+geilefunktion(gang)
+
+
+
+
---
+++ Easy_Fit.R
@@ -0,0 +1,41 @@
+#Data package
+library(car)
+#One favorit Plot package
+library(ggplot2) 
+#Data handling package
+library(MASS) 
+#Boosting tree Alg.
+library(xgboost)
+#An other nice plot package
+library(rbokeh)
+
+newdata = Prestige[,c(1:2)]
+
+# fit a linear model and run a summary of its results.
+set.seed(1)
+education.c = scale(newdata$education, center=TRUE, scale=FALSE)
+mod = lm(income ~ education.c, data = newdata)
+summary(mod)
+
+# visualize the model results.
+qplot(education.c, income, data = newdata, main = "Relationship between Income and Education") +
+  stat_smooth(method="lm", col="red") +
+  scale_y_continuous(breaks = c(1000, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000, 18000, 20000, 25000), minor_breaks = NULL)
+
+plot(mod, pch=16, which=1)
+train.pred_lm <- predict(mod)
+
+train.data <- data.matrix(scale(newdata$education, center=TRUE, scale=FALSE))
+train.label <- data.matrix(newdata$income)
+mod2 <- xgboost(data = train.data, label = train.label, max_depth = 10, eta = 0.015, nthread = 4, 
+          nrounds = 1000, objective = "reg:linear")
+
+train.pred <- predict(mod2, train.data)
+
+figure() %>%
+  ly_points(data.frame(train.data,train.label),color="gray") %>%
+  ly_points(data.frame(train.data,train.pred),color = "green") %>%
+  ly_points(data.frame(train.data,train.pred_lm))
+
+# 
+
