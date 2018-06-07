11e77f9ebb4c9149324dbab4b9688e86d521a152
Benjamin Seeber <Benjamin.Seeber@five1.de>
2018-06-07 12:17:18 +0200

    Added the base for r intro

---
+++ Basics.R
@@ -0,0 +1,9 @@
+#Die Basis für ein R Skript ist es auch ein paar Kommentare zu machen
+#Wie z.B. das jetzt eine Library Installiert wird.
+install.packages('rbokeh')
+install.packages('xgboost')
+
+#Ups... jetzt sind doch ein paar mehr Packete installiert
+#Dann können diese jetzt auch geladen werden
+library(rbokeh)
+library(xgboost)
